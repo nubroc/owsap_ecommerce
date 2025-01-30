f22f38c24edae0566ddfe16ced4b4d8a49748754
arcabob <159237376+arca-bob@users.noreply.github.com>
2025-01-30 12:28:42 +0100

    Update ProduitController.php

--- controllers/ProduitController.php
+++ controllers/ProduitController.php
@@ -2,20 +2,15 @@
 
 include_once __DIR__ . '/../models/Article.php';
 
-
-class ProduitController
-{
-    public function catalogue()
-{
+class ProduitController {
+    public function catalogue() {
         $catalogue = Article::getAll();
+        //var_dump($catalogue);
+        $this->render('catalogue', ['catalogue' => $catalogue]);
+    }
 
-    $this->render('catalogue', ['catalogue' => $catalogue]);
-}
-
-
-    private function render($view, $data = [])
-{
+    private function render($view, $data = []) {
         extract($data);
         include __DIR__ . "/../views/{$view}.php";
-}
+    }
 }
