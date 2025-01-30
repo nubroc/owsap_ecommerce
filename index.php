4753afa7bbc882cdb2ddec1fcb149b62b9a00bbe
arcabob <159237376+arca-bob@users.noreply.github.com>
2025-01-30 12:36:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 session_start();
 
 require_once 'config/Database.php';
-require_once 'config/router.php';
-
+require_once 'controllers/HomeController.php';
+require_once 'controllers/LoginController.php';
+require_once 'controllers/ProduitController.php';
 
 
 $database = new Database();
 $db = $database->getConnection();
 
 
+$page = $_GET['page'] ?? 'home';
 
+if ($page === 'home') {
+    $homeController = new HomeController();
+    $homeController->index();
+} elseif ($page === 'login') {
+    $authController = new LoginController();
+    $authController->login();
+} elseif ($page === 'logout') {
+    $authController = new LoginController();
+    $authController->logout();
+} elseif ($page === 'catalogue') {
+    $produitController = new ProduitController();
+    $produitController->catalogue(); 
+} else {
+    echo "Page non trouvée";
+}
 ?>
